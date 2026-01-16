23171a96d6a8ab29aa34790bd6264272f4393580
Siddharth Bade <siddharthbade29@gmail.com>
2026-01-16 17:49:34 +0530

    my own website link

--- usingSelenium.py
+++ usingSelenium.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 d=webdriver.Edge()
-d.get("https://www.flipkart.com/")
+d.get("https://siddhya2911.github.io/weather_app/")
 time.sleep(4)
-title=d.find_element(By.TAG_NAME,"h2")
+title=d.find_element(By.TAG_NAME,"h1")
+titlee=d.find_element(By.TAG_NAME,"h2")
 print(title.text)
+print(titlee.text)
 d.quit()
